a9cb0873d330d9b6bd0054ddee62efc5cd210a96
Jhonaiza Gede <gedejhonaiza@gmail.com>
2026-09-24 02:52:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -640,7 +640,7 @@ const height = section.offsetHeight;
     ========================================================= */
 
     const loaderDuration =
-        6500;
+        6000;
 
 
     const loaderStartTime =
